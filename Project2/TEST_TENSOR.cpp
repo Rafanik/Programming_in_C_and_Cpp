71969bd7fe971711a9809302caa6c97c81503870
Rafanik <37025865+Rafanik@users.noreply.github.com>
2018-04-20 23:12:18 +0200

    dodanie komentarzy do kodu źródłowego

--- Project2/TEST_TENSOR.cpp
+++ Project2/TEST_TENSOR.cpp
@@ -1,37 +1,37 @@
 #include <iostream>
 #include "TEST_TENSOR.h"
+//definicje używanych napisów
 #define START "Rozpoczynam testowanie metod dla typu: "
 #define TEST "Test metody: "
 #define WYNIK " zakonczyl sie wynikiem: "
 
-
-void testInt(){
+//Scenariusz testowy dla poszczególnych klas
+void testInt(){//test klasy int
 cout<<START<<"int"<<endl<<endl;
-Tensor<int> t1(1,1,2);
+Tensor<int> t1(1,1,2);//stworzenie zerowych tensorów o odpowiednich wymiarach
 Tensor<int> t2(1,1,2);
 Tensor<int> wynik(1,1,2);
 TestTensor<int> test(t1, t2, wynik);
-t1.change(3,0,0,0);
-cout<<TEST<<"Change i Read"<<WYNIK<<test.testChangeRead(7)<<endl<<endl;
-test.reset(2,3,4,5);
-test.setResult(6,8);
-cout<<TEST<<"Op+"<<WYNIK<<test.testOpPlus()<<endl<<endl;
+cout<<TEST<<"Change i Read"<<WYNIK<<test.testChangeRead(7)<<endl<<endl;//test metod change oraz read
+test.reset(2,3,4,5);//ustawienie wartości pól tensorów, na których testowane będzie działanie metody operator+
+test.setResult(6,8);//ustawienie przewidywanego wyniku działania metody operator+ /+= w polach tensora wynik
+cout<<TEST<<"Op+"<<WYNIK<<test.testOpPlus()<<endl<<endl;// przeprowadzenie testu operator+ oraz operator+= i wyświetlenie wyniku testu
 cout<<TEST<<"Op+="<<WYNIK<<test.testOpPlusEq()<<endl<<endl;
-test.reset(8,3,4,5);
-test.setResult(4,-2);
-cout<<TEST<<"Op-"<<WYNIK<<test.testOpMin()<<endl<<endl;
+test.reset(8,3,4,5);//ustawienie wartości pól tensorów, na których testowane będzie działanie metody operator- /-=
+test.setResult(4,-2);//ustawienie przewidywanego wyniku działania metody operator- /-= w polach tensora wynik
+cout<<TEST<<"Op-"<<WYNIK<<test.testOpMin()<<endl<<endl;// przeprowadzenie testu operator- oraz operator-= i wyświetlenie wyniku testu
 cout<<TEST<<"Op-="<<WYNIK<<test.testOpMinEq()<<endl<<endl;
-test.reset(3,3,4,5);
-test.setResult(12,15);
-cout<<TEST<<"Op*"<<WYNIK<<test.testOpMul()<<endl<<endl;
+test.reset(3,3,4,5);//ustawienie wartości pól tensorów, na których testowane będzie działanie metody operator* /*=
+test.setResult(12,15);//ustawienie przewidywanego wyniku działania metody operator* /*= w polach tensora wynik
+cout<<TEST<<"Op*"<<WYNIK<<test.testOpMul()<<endl<<endl;// przeprowadzenie testu operator* oraz operator*= i wyświetlenie wyniku testu
 cout<<TEST<<"Op*="<<WYNIK<<test.testOpMulEq()<<endl<<endl;
-test.reset(7,9,7,9);
-cout<<TEST<<"Op=="<<WYNIK<<test.testOpEq(1)<<endl<<endl;
+test.reset(7,9,7,9);//ustawienie wartości pól tensorów, na których testowane będzie działanie metody operator=
+cout<<TEST<<"Op=="<<WYNIK<<test.testOpEq(1)<<endl<<endl;// przeprowadzenie testu operator==/ !=/ = i wyświetlenie wyniku testu
 cout<<TEST<<"Op!="<<WYNIK<<test.testOpDif(0)<<endl<<endl;
 cout<<TEST<<"Op="<<WYNIK<<test.testOpPrz()<<endl<<endl;
 
 }
-
+//schemat testowania analogiczny jak dla klasy int
 void testDouble(){
 cout<<START<<"double"<<endl<<endl;
 Tensor<double> t1(1,1,2);
@@ -55,5 +55,4 @@ test.reset(7.34,9.33,7.34,9.33);
 cout<<TEST<<"Op=="<<WYNIK<<test.testOpEq(1)<<endl<<endl;
 cout<<TEST<<"Op!="<<WYNIK<<test.testOpDif(0)<<endl<<endl;
 cout<<TEST<<"Op="<<WYNIK<<test.testOpPrz()<<endl<<endl;
-
 }
